baf40d93cf0814e0b7ce48b8c34ed600cf34dd8c
Tobias <tobi@mail.de>
2014-08-29 17:33:01 +0200

    Change Template

--- application.js
+++ application.js
@@ -1,63 +1,71 @@
-var init = function(JSONdata){
+/*global Backbone*/
 
+window.init = function(JSONdata, postHTML) {
 	var BlogItem = Backbone.Model.extend({ });
 	var BlogCollection = Backbone.Collection.extend({ model: BlogItem });
 	var blogCollection = new BlogCollection();
+
+
 
 	var BlogView = Backbone.View.extend({
 
 		className: 'blogpost',
 
 		events: {
-			'mouseover': 'highLight',  
+			'mouseover': 'highLight',
 			'mouseout' : 'unhighLight',
 			'click .edit': 'editPost',
 			'click .submitchange': 'changePost'
 		},
 
-		highLight: function(){
-			this.el.style.backgroundColor = "yellow";
-			$(this.el).find('button')[0].style.display='inline';
+		highLight: function() {
+			this.el.style.backgroundColor = 'yellow';
+			$(this.el).find('button')[0].style.display = 'inline';
 		},
 
-		unhighLight: function(){
-			this.el.style.backgroundColor = "transparent";
-			$(this.el).find('button')[0].style.display='none';
+		unhighLight: function() {
+			this.el.style.backgroundColor = 'transparent';
+			$(this.el).find('button')[0].style.display = 'none';
 		},
 
-		editPost: function(){
-			var title = '"' + this.model.get("title") + '"';
-			var description = '"' + this.model.get("description") + '"';
-			var author = '"' + this.model.get("author") + '"';
+		editPost: function() {
+			var title = '"' + this.model.get('title') + '"';
+			var description = '"' + this.model.get('description') + '"';
+			var author = '"' + this.model.get('author') + '"';
 
 			$(this.el).find('h3').html(
-				'Title <input type="text" class="title" style="margin:0px; padding:0px;"  value=' + title + '>');
+				'Title <input type="text" class="title" style="margin:0px;' +
+				'padding:0px;"  value=' + title + '>');
+
 			$(this.el).find('.descr').html(
-				'Description <input type="text" style="margin:0px; padding:0px;" class="description" value=' + description+ '>');
+				'Description <input type="text" class= "description" style="margin:0px;' +
+				'padding:0px;"  value=' + description + '>');
+
 			$(this.el).find('.auth').html(
-				'Author <input type="text" class="author" style="margin:0px; padding:0px;"  value=' + author + '>');
+				'Author <input type="text" class="author" style="margin:0px;' +
+				'padding:0px;"  value=' + author + '>');
+
 			$(this.el).find('button').remove();
-			$(this.el).append('<button class="submitchange" style="margin: 0px; padding:0px; margin-left:10px;"> Submit Changes </button>');
+			$(this.el).append('<button class="submitchange" style="margin: 0px;' +
+			'padding:0px; margin-left:10px;"> Submit Changes </button>');
 		},
 
-		changePost: function(){
+		changePost: function() {
 			var title = $(this.el).find('.title')[0].value;
 			var description = $(this.el).find('.description')[0].value;
 			var author = $(this.el).find('.author')[0].value;
 			this.model.set({
-					title: title,
-					description:description,
-					author:author
-			               });
+				title: title,
+				description: description,
+				author:author
+			});
 			this.render();
 
 		},
 
-		template: _.template('<h3> <%= title %></h3>' +
-			                 '<span class="descr"> <%= description %> </span>' +  
-			                 '<button class="edit" style="display:none; margin:0px; margin-left:10px; padding:0px;"> Edit Post </button>' +
-			                 '</br> </br> <span class="auth"> <%= author %></span>' ),
-		render: function(){
+		template: _.template(postHTML),
+
+		render: function() {
 
 			var attributes = this.model.toJSON();
 			this.$el.html(this.template(attributes));
@@ -65,22 +73,63 @@ var init = function(JSONdata){
 	});
 
 
+
+
+	var BlogEditView = Backbone.View.extend({
+
+		template: _.template(
+
+			'<form id="newentry">' +
+			'Title: <input type="text" name="title" class="input" value= "<%=title%>" ><br>' +
+
+			'Description: <input type="text" name="description"' +
+			'size="40" value = " <%= description %> " class="input"> <br>' +
+
+			'Author: <input type="text" name="author" value= " <%=author%> " class="input"> <br>' +
+
+			'<input type="submit" value="Submit" id ="submit">' +
+			'</form>'
+		),
+
+		render: function() {
+			var attributes = this.model.toJSON();
+			this.$el.html(this.template(attributes));
+
+		}
+
+	});
+
+	var emptyView = new BlogItem(
+		{title:'', description:'', author:'', id:'emtpyform'}
+	);
+
+	console.log(emptyView);
+
+	var blogEditView = new BlogEditView({model: emptyView });
+	blogEditView.render();
+
+	$('#form').html(blogEditView.el);
+
+
 	blogCollection.reset(JSONdata);
+
+	console.log(blogCollection.length);
 
 
 	var BlogCollectionView = Backbone.View.extend({
 
-		initialize: function(){
+		initialize: function() {
 
 			this.collection.on('add', this.addOne, this);
 		},
 
-		render: function(){
+		render: function() {
 			this.collection.forEach(this.addOne, this);
 		},
 
-		addOne: function(blogItem){
+		addOne: function(blogItem) {
 			var blogView  = new BlogView({model: blogItem});
+			console.log(blogItem);
 			blogView.render();
 			this.$el.append(blogView.el);
 		}
@@ -89,30 +138,35 @@ var init = function(JSONdata){
 	var blogCollectionView = new BlogCollectionView({collection: blogCollection});
 
 
-	var refreshBlog = function(){
 
-	blogCollectionView.render();
 
-	$('#blogroll').html(blogCollectionView.el);
+	var refreshBlog = function() {
+
+		blogCollectionView.render();
+
+		$('#blogroll').html(blogCollectionView.el);
 
 	};
 
-    refreshBlog();
-    
+	refreshBlog();
 
-	$('#newentry').submit( function(){
+
+
+
+	$('#newentry').submit( function() {
 
 		var title = this.elements[0].value;
 		var description = this.elements[1].value;
-		var author = this.elements[2].value; 
+		var author = this.elements[2].value;
+		var id = blogCollection.length;
 
 		$('.input').val('');
 
 		var newblogItem = new BlogItem(
-			{title: title, description: description, author:author});
+			{title: title, description: description, author:author, id:id});
 
 		blogCollection.add(newblogItem);
-		
+
 		return false;
 
 	});
